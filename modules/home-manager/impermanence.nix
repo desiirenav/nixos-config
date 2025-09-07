be09aae5d19d57b78d399f630f0c753c19d06787
desiirenav <desiirenav@hotmail.com>
2025-09-07 18:29:11 -0400

    new install

---
+++ modules/home-manager/impermanence.nix
@@ -0,0 +1,31 @@
+{inputs, lib, pkgs, ...}: {
+
+  imports = [
+    inputs.impermanence.homeManagerModules.impermanence
+  ];
+
+  home.persistence."/persist/home" = {
+    directories = [
+      "Downloads"
+      "Music"
+      "Pictures"
+      "Documents"
+      "Videos"
+      "Games"
+      "Projects"
+      ".ssh"
+      ".zen"
+      ".config/ignis"
+      ".local/share/Steam"
+#      ".local/share/anime-game-launcher"
+      ".local/share/sleepy-launcher"
+      ".config/discord"
+      ".config/Vencord"
+    ];
+    files = [
+      ".bash_history"
+      ".config/systemsettingsrc"
+    ];
+    allowOther = true;
+  };
+}
