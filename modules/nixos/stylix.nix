75afc5f7900360cf117aff28e59fcd19053cdec5
desiirenav <desiirenav@hotmail.com>
2025-09-09 15:30:05 -0400

    gruvbox

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -11,29 +11,12 @@
   stylix = {
     autoEnable = true;
     enable = true;
-    image = ./../../assets/wallpapers/sky.png;
-    base16Scheme = {
-      base00 = "0B0E14";
-      base01 = "131721";
-      base02 = "272D38";
-      base03 = "3E4B59";
-      base04 = "BFBDB6";
-      base05 = "E6E1CF";
-      base06 = "E6E1CF";
-      base07 = "F3F4F5";
-      base08 = "F07178";
-      base09 = "FF8F40";
-      base0A = "FFB454";
-      base0B = "B8CC52";
-      base0C = "95E6CB";
-      base0D = "59C2FF";
-      base0E = "D2A6FF";
-      base0F = "E6B673";
-    };
+    image = ./../../assets/wallpapers/gruvbox.png;
+     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     polarity = "dark";
     cursor = {
-      package = pkgs.phinger-cursors;
-      name = "phinger-cursors-dark";
+      package = pkgs.capitaine-cursors-themed;
+      name = "Capitaine Cursors (Gruvbox)";
       size = 22;
     };
     fonts = {
