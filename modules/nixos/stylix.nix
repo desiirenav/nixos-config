562fecb2eb960fe3cfa87554f8b01f308a656bf4
desiirenav <desiirenav@hotmail.com>
2025-09-09 20:55:24 +0000

    Update stylix.nix

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -19,20 +19,20 @@
       name = "Capitaine Cursors (Gruvbox)";
       size = 22;
     };
-    fonts = {
-      serif = {
-        package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
-        name = "SFProDisplay Nerd Font";
-      };
-      sansSerif = {
-        package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
-        name = "SFProDisplay Nerd Font";
-      };
-      monospace = {
-        package = pkgs.sf-mono-liga-bin;
-        name = "Liga SFMono Nerd Font";
-      };
-    };
+    #fonts = {
+    #  serif = {
+    #    package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
+    #    name = "SFProDisplay Nerd Font";
+    # };
+    #  sansSerif = {
+    #    package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
+    #    name = "SFProDisplay Nerd Font";
+    #  };
+    #  monospace = {
+    #    package = pkgs.sf-mono-liga-bin;
+    #    name = "Liga SFMono Nerd Font";
+    #  };
+    #};
   };
 }
 
