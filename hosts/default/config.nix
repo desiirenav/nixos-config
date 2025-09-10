c9d6108d88a816b2b008e137d033ed84c098f12c
desiirenav <desiirenav@hotmail.com>
2025-09-10 12:12:07 -0400

    Update config.nix

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -43,8 +43,7 @@
   environment.variables.EDITOR = "nvim";
 
   # GDM
-  #services.displayManager.gdm.enable = true;
-  #services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.enable = true;
   #services.desktopManager.gnome.enable = true;
   #services.gnome.core-apps.enable = false;
   #services.gnome.core-developer-tools.enable = false;
