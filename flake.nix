b5c5bf4ebd0855a8d05a6e0a4df49abf20574031
desiirenav <desiirenav@hotmail.com>
2025-09-10 01:22:09 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -12,9 +12,11 @@
     aagl.url = "github:ezKEa/aagl-gtk-on-nix";
     aagl.inputs.nixpkgs.follows = "nixpkgs";
     nixcord.url = "github:kaylorben/nixcord";
+    niri.url = "github:sodiboo/niri-flake";
+    niri-unstable.url = "github:YaLTeR/niri";
     impermanence.url = "github:nix-community/impermanence";
     nvim-config.url = "github:desiirenav/nvim-config";
-    #apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
+    apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
     stylix.url = "github:danth/stylix";
     anyrun = {
       url = "github:anyrun-org/anyrun";
@@ -28,14 +30,10 @@
       url = "github:ignis-sh/ignis";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    niri = {
-      url = "github:sodiboo/niri-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
+    sf-mono-liga-src = {
+      url = "github:shaunsingh/SFMono-Nerd-Font-Ligaturized";
+      flake = false;
     };
-    #sf-mono-liga-src = {
-      #url = "github:shaunsingh/SFMono-Nerd-Font-Ligaturized";
-      #flake = false;
-    #};
     zen-browser = {
       url = "github:pfaj/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
